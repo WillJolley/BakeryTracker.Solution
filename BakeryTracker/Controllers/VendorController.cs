12d50ea370249e2dc97941370505cd148e092d5a
WillJolley <yeswilljolley@gmail.com>
2023-09-23 21:24:00 -0700

    add home controller and vendor controller

---
+++ BakeryTracker/Controllers/VendorController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+
+namespace BakeryTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
+    }
+  }
+}
